ac19c325aa1b39f52f958195e12c4f8d47443bfc
Giwan Persaud <giwan@knowble.nl>
2019-03-12 16:41:26 +0100

    renames dialog to index file

--- src/client/components/docs-editor.jsx
+++ src/client/components/docs-editor.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 export default class DocEditor extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <h1>hello world</h1>
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>hello world</h1>
+            </div>
+        );
+    }
 }
--- src/server/code.js
+++ src/server/code.js
@@ -1,6 +1,20 @@
-import * as publicFunctions from './docs-utilities.js';
+// Use ES6/7 code
+const onOpen = (e) =>
+    DocumentApp.getUi() // Or SheetsApp or FormApp.
+        .createAddonMenu()
+        .addItem('360AI', 'showSidebar')
+        .addToUi();
+
+const onInstall = (e) => onOpen(e);
+
+const showSidebar = () => {
+    const ui = HtmlService.createHtmlOutputFromFile('index.html').setTitle(
+        'Author Insights with EDIA\'s 360AI '
+    );
+    return DocumentApp.getUi().showSidebar(ui);
+};
 
 // Expose public functions
-global.onOpen = publicFunctions.onOpen;
-global.onInstall = publicFunctions.onInstall;
-global.showSidebar = publicFunctions.showSidebar;
+global.onOpen = onOpen;
+global.onInstall = onInstall;
+global.showSidebar = showSidebar;
