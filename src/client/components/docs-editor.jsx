2ba8396cb689cc0a74f459cded6597fe902fa00b
Giwan Persaud <giwan@knowble.nl>
2019-03-12 16:54:38 +0100

    styling using css

--- src/client/components/docs-editor.jsx
+++ src/client/components/docs-editor.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import '../styles.css';
 export default class DocEditor extends React.Component {
     render() {
         return (
-            <div>
+            <div className="ai-container">
                 <h1>hello world</h1>
             </div>
         );
